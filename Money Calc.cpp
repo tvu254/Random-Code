aa5dd6d6c9e5625511979feb58488b2386c16d44
tvu254 <90804015+tvu254@users.noreply.github.com>
2024-11-25 00:13:36 -0600

    add ppl and dish func

--- Money Calc.cpp
+++ Money Calc.cpp
@@ -31,7 +31,36 @@ vector<Dish> getDishes(int numDishes, int numPpl)
 {
     vector<Dish> dishes(numDishes);
 
-    for (int i = 0 )
+    for (int i = 0; i <numDishes; ++i)
+    {
+        cout << "\nDish " << i + i << ":" << endl;
+
+        cout << "Enter the name of the dish: ";
+        getline(cin, dishes[i].name);
+
+        cout << "Enter the price of the dish: $";
+        cin >> dishes[i].price;
+
+        cout << "Is this dish 'individual' or 'shared'? ";
+        cin >> dishes[i].type;
+
+         if (dishes[i].type == "individual") {
+            cout << "Enter the person number (1-" << numPpl << ") to assign this dish to: ";
+            int personIndex;
+            cin >> personIndex;
+            dishes[i].assignedPpl.push_back(personIndex - 1); 
+        } else if (dishes[i].type == "shared") {
+            cout << "This dish will be split among everyone." << endl;
+            for (int j = 0; j < numPpl; ++j) {
+                dishes[i].assignedPpl.push_back(j);
+            }
+        }
+
+        cin.ignore(); 
+    }
+
+    return dishes;
+    }
 }
 
 int main()
